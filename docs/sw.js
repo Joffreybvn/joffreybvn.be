2875441c882d0a2f19b684da0af6bbd0a749bcfd
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-05-19 00:27:26 +0200

    Responsiveness for very small devices corrected

--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-18c2e0ab0a649cced6ab.js"
+    "url": "webpack-runtime-130dc88f1f8051ed40f7.js"
   },
   {
     "url": "framework-c7fa3a9a091538155213.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2ee4205c96d9083396ef385e13cca5b6"
+    "revision": "4fddf6dbd5f21d7cff697f9081633d23"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9de147692d3f7c3f8c6a.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
     "url": "polyfill-0eb60cc0e14bb6ff2023.js"
   },
   {
-    "url": "styles.dd750ad8120214290b64.css"
+    "url": "styles.2287aa5ff47cef34d500.css"
   },
   {
     "url": "c169b6902947a77fec05096cb4da441bfcc3d6f1-adf73d11e4ac10b4a80c.js"
@@ -60,7 +60,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9ffb8f04e610441db5ed4c37fcafe285"
+    "revision": "a4866c8a774f4f27d11a276aa0ecbb40"
   },
   {
     "url": "manifest.webmanifest",
